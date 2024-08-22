3a2fbccab4b18fbd87d68ee5861805087b85b463
Ajinkyaj21 <ajinkya.jagadale@flairminds.com>
2024-08-22 12:14:44 +0530

    minor code change

--- middlewares/morgan.middleware.js
+++ middlewares/morgan.middleware.js
@@ -1,13 +1,13 @@
-const morgan = require('morgan');
-const { httpLogger } = require('../config/logger');
+//const morgan = require('morgan');
+//const { httpLogger } = require('../config/logger');
 
-const stream = {
-    write: (message) => httpLogger.info(message)
-};
+//const stream = {
+//    write: (message) => httpLogger.info(message)
+//};
 
-const morganMiddleware = morgan(
-    ":method :url :status :response-time ms - :res[content-length]",
-    { stream }
-);
+//const morganMiddleware = morgan(
+//    ":method :url :status :response-time ms - :res[content-length]",
+//    { stream }
+//);
 
-module.exports = morganMiddleware;
+//module.exports = morganMiddleware;
--- utils/server.js
+++ utils/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morganMiddleware = require('../middlewares/morgan.middleware');
+//const morganMiddleware = require('../middlewares/morgan.middleware');
 const { routesMap } = require('../routes');
 
 
@@ -10,7 +10,7 @@ function createServer() {
     app.use(cors({origin: true, credentials: true}));
     app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true })); 
-    app.use(morganMiddleware);
+    //app.use(morganMiddleware);
 
     Object.keys(routesMap).forEach((key) => app.use(key, routesMap[key]))
 
