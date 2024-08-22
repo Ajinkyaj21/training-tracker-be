d8dfaaf4d4f831aaba32c79a9dc7d821226da489
Ajinkyaj21 <ajinkya.jagadale@flairminds.com>
2024-08-22 16:14:48 +0530

    added fs

--- routes/technologyRouter.js
+++ routes/technologyRouter.js
@@ -17,14 +17,27 @@ const { userAuthMiddleware } = require("../middlewares/userMiddleware");
 //});
 
 const storage = multer.diskStorage({
+
     destination: (req, file, cb) => {
-        // Correctly join __dirname with 'uploads' to specify the upload directory
-        cb(null, path.join( __dirname, 'uploads'));
+
+        const dir = path.join(__dirname, '..', 'uploads');
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
+        cb(null, dir);
+
     },
+
     filename: (req, file, cb) => {
-        // Use the original file name for storage
-        cb(null, file.originalname);
+
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
+
+        cb(null, file.originalname + '-' + uniqueSuffix);
+
     }
+
 });
 
 
