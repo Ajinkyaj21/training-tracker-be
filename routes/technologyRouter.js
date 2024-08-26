f90f747f82189a6595a40dc4d448c12cbcccae39
Omkar-Hirve-Flairminds <omkar.hirve@flairminds.com>
2024-08-26 11:47:54 +0530

    multer dynamic location

--- routes/technologyRouter.js
+++ routes/technologyRouter.js
@@ -18,10 +18,10 @@ const { userAuthMiddleware } = require("../middlewares/userMiddleware");
 //});
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+      cb(null, path.join(__dirname, '..'));
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+      cb(null, file.originalname + `${Date.now() + '-' + Math.round(Math.random() * 1E9)}`);  
   }
 });
 
